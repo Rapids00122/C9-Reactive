82e31c3b5af638fcdc47b1359c630fb383bc7f96
Richard Ford <duracell00122@gmail.com>
2024-04-09 10:58:44 -0400

    Finished API XML fetching

--- app.js
+++ app.js
@@ -12,8 +12,18 @@ const app = Vue.createApp({
             try {
                 const response = await fetch('https://cdn.animenewsnetwork.com/encyclopedia/api.xml?anime=4658')
                 const xmlData = await response.text()
-                console.log(xmlData)
-                this.animeData = xmlData
+                let parser = new DOMParser()
+                let xmlDOM = parser.parseFromString(xmlData, 'application/xml')
+
+                const infoNodes = xmlDOM.querySelectorAll('info')
+                const episodeCountNode = infoNodes[12]
+                const episodeCount = episodeCountNode.textContent.trim()
+
+                const nameAttribute = xmlDOM.querySelector('anime').getAttribute('name')
+                console.log(nameAttribute)
+                const name = nameAttribute.trim()
+
+                this.animeData = { name, episodeCount }
             } catch (error) {
                 console.error('Error fetching anime data:', error)
             }
